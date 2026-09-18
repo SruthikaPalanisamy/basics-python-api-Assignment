6ded182585a394b74f4a1f076c4127a01fc42af7
Sruthika Palanisamy <sruthi38655@gmail.com>
2026-09-18 10:38:58 +0000

    new commit

---
+++ library_management/library_management/doctype/learn_tree/learn_tree.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Me and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Learn_Tree", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library_management/library_management/doctype/learn_tree/learn_tree.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Me and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Learn_Tree(NestedSet):
+	pass
